c4ed2a327a312562d9af0084b9d85948afb78e08
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-03-19 11:40:30 +0100

    REFACTORS contact form to integrate Zod validation and handle form submission, missing errors

--- storefront/components/contact/ContactForm.tsx
+++ storefront/components/contact/ContactForm.tsx
@@ -1,25 +1,83 @@
-// import { zodResolver } from "@hookform/resolvers/zod";
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ContactSchema, ContactFormInput } from "@/schemas/contactSchema";
+
 export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ContactFormInput>({
+    resolver: zodResolver(ContactSchema),
+    values: {
+      name: "",
+      subject: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const onSubmit = async (data: ContactFormInput) => {
+    console.log("form submitted:", data);
+    reset();
+    alert("you have submitted the contact form");
+  };
+
   return (
     <div className="flex ">
-      <form className="flex flex-col gap-4 ">
-     
+        
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 ">
         {/* name */}
         <label htmlFor="name"> Name:</label>
-        <input className="border" id="name" type="text" name="name" required />
-       
+        <input
+          className="border"
+          {...register("name")}
+          id="name"
+          type="text"
+          name="name"
+          required
+        /> 
+        {errors.name && (<span>{errors.name.message}</span>)
+
+        }
+        
+
         {/* subject */}
-         <label htmlFor="subject">Subject:</label>
-        <input className="border" id="subject" type="text" name="subject" required />
-       
+        <label htmlFor="subject">Subject:</label>
+        <input
+          className="border"
+          {...register("subject")} 
+          id="subject"
+          type="text"
+          name="subject"
+          required
+        />
+
         {/* email */}
-         <label htmlFor="email"> Email:</label>
-        <input className="border" id="email" type="email" name="email" required />
-       
+        <label htmlFor="email"> Email:</label>
+        <input
+          className="border"
+          {...register("email")} 
+          id="email"
+          type="email"
+          name="email"
+          required
+        />
+
         {/* message */}
-         <label htmlFor="message"> Message:</label>
-        <input className="border" id="message" type="text" name="message" required />
-       <button className="flex bg-amber-200 rounded-lg"> Submit </button>
+        <label htmlFor="message"> Message:</label>
+        <input
+          className="border"
+          {...register("message")} 
+          id="message"
+          type="text"
+          name="message"
+          required
+        />
+        <button className="flex bg-amber-200 rounded-lg" type="submit"> Submit </button>
       </form>
     </div>
   );
