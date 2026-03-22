d784764afd5f1d58b04b23d5692c849af7b6f1ce
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-03-22 11:36:02 +0100

    REFACTOR price

--- storefront/components/ui/card/ProductDetailsCard.tsx
+++ storefront/components/ui/card/ProductDetailsCard.tsx
@@ -5,40 +5,46 @@ import TagsSection from "@/components/product/Tags";
 import RatingSection from "@/components/product/Rating";
 // card for product details should contain:
 
-
 // price
 // price discount
 // rating
 //tags need to be mapped out
 
-
 export default function ProductDetailsCard({ product }: { product: Product }) {
+      const hasDiscount = product.discountedPrice < product.price;
+  const discountPercent = hasDiscount
+    ? Math.round(
+        ((product.price - product.discountedPrice) / product.price) * 100
+      )
+    : 0;
   return (
-     <div>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      
+    <div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <div className="relative">
           <Image
-          className="w-full"
+            className="w-full"
             src={product.image.url}
             alt={product.image.alt}
-           width={700}
-           height={700}
-        
-          />
-       </div>
-       <div className="px-6 py-4">
-        <h2 className="font-bold text-xl mb-2">{product.title}</h2>
-        <RatingSection rating={product.rating}/>
-        <p className="text-gray-700 text-base">
-          {product.description}
-        </p>
-        <TagsSection tags={product.tags}/>
-            
-        <div className="">
-          {product.price}
-          {product.discountedPrice}
+            width={700}
+            height={700}
+          />  {hasDiscount && (
+          <div className="absolute top-0 left-0 bg-amber-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">
+            -{discountPercent}%
+          </div>
+        )}
         </div>
+        <div className="px-6 py-4">
+          <h2 className="font-bold text-xl mb-2">{product.title}</h2>
+          <RatingSection rating={product.rating} />
+          <p className="text-gray-700 text-base">{product.description}</p>
+          <TagsSection tags={product.tags} />
+
+          <div className="flex">
+           
+            <div className="flex-1">{product.discountedPrice.toFixed(2)}</div> 
+            {hasDiscount && (
+            <div className="flex-4 line-through"> {product.price.toFixed(2)}</div>
+          )}</div>
         </div>
         <div>
           <button
@@ -48,8 +54,9 @@ export default function ProductDetailsCard({ product }: { product: Product }) {
             Add to Cart
           </button>
         </div>
-         </div>
-      <ReviewSection reviews={product.reviews}/>
+      </div>
+
+      <ReviewSection reviews={product.reviews} />
     </div>
   );
 }
