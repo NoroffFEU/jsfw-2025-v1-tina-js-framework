ec2face6470546829b18807edb9d3a5aa3e2088f
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-03-23 10:42:45 +0100

    ADDS structure for cart

---
+++ storefront/app/cart/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default async function CartPage() {
+
+    
+
+    return (
+      <div className="">
+        <h1 className="text-2xl font-bold">CART CHECK OUT</h1>
+        
+      </div>
+    )}
