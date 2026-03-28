db6a28c71b53c94150aeb85b749a4183f7df1cbc
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-03-28 17:41:16 +0100

    FIX padding and container issues

--- storefront/app/contact/success/page.tsx
+++ storefront/app/contact/success/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 export default function ContactFormSuccessPage() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 py-18  text-center">
+    <div className="flex flex-col items-center justify-center gap-6 max-w-3xl mx-auto px-4 py-12 text-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -17,7 +17,9 @@ export default function ContactFormSuccessPage() {
           d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
         />
       </svg>
-      <h1 className="font-heading text-2xl ">Thank you for your request!</h1>
+      <h1 className="font-heading text-2xl font-bold text-indigo-950">
+        Thank you for your request!
+      </h1>
       <p className="text-sm">
         We have received your contact form and will reach out to you as soon as
         possible.
