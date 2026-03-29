aea87c4b5e02a3c0911030d4baa2e7fe671175d0
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-03-29 16:41:55 +0200

    ADDS Toast, not finished

---
+++ storefront/components/ui/Toast.tsx
@@ -0,0 +1,17 @@
+// CREATE A TOAST STORE
+
+export default function Toast {
+    const toastMessage = //use the toast store
+    if !toastMessage return null;
+
+    return (
+        <div>
+            {toastMessage}
+        </div>
+
+    )
+}
+
+//ADD toast to button that triggers when adding to cart
+//ADD to remove item in cart
+//
